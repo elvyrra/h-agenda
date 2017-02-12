baf68d0daede874e00fa335abc4ba8fb62250755
Thaon <jthaon@elvyrra.com>
2017-02-12 09:32:55 +0100

    bug add event from calendar

--- static/js/agenda-events-calendar.js
+++ static/js/agenda-events-calendar.js
@@ -4,10 +4,8 @@
 'use strict';
 
 require(['app', 'jquery'], function() {
-
-	$('[name="data-cal-date"]').click(function(){
-    //$('.cal-cell').click(function(){
-    	var date = this.firstElementChild.firstElementChild.getAttribute('data-cal-date');
+    $('.cal-month-day').click(function(){
+    	var date = this.firstElementChild.getAttribute('data-cal-date');
     	app.dialog(app.getUri('h-agenda-edit-event', {id: 0}) + "?date=" + date);
     });
 })();
